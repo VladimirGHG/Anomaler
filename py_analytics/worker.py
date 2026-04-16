a1c9101dbe6c5ac2ec280ad0249633d3929ad16e
Vladimir <vlad2005garag@Gmail.com>
2026-04-16 23:20:13 +0200

    feat(Model snapshoting mechanism)

--- py_analytics/worker.py
+++ py_analytics/worker.py
@@ -3,8 +3,11 @@ import zmq
 from .models import AnomalyModel
 from sklearn.utils import shuffle
 import sys
+import time
 import json
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODELS_DIR = os.path.join(BASE_DIR, "models")
 
 class ZMQWorker:
     """Worker process that receives data batches via ZeroMQ, processes them with the given anomaly detection strategy, and reports results."""
@@ -39,10 +42,25 @@ class ZMQWorker:
                     all_new_values.extend([p["value"] for p in packet["datapoints"]])
 
                 # Process everything in one go
+                results = []
                 if all_new_values:
                     results = self.strategy.process_batch(all_new_values)
                     # self.report(results)
-                    self.calculate_precision(results, batch_of_packets, results[-1].get("status"))
+
+                if self.strategy.last_save_time is None:
+                    self.strategy.last_save_time = time.time()
+                elif time.time() - self.strategy.last_save_time > 20: # Save every 5 minutes (will not be hardcoded in future)
+                    if self.strategy.model_snapshot >= 5: # Keep only last 5 snapshots (will not be hardcoded in future)
+                        self.strategy.model_snapshot = 0
+                        print("[DISK] Reached max snapshots. Overwriting from model_0.pkl")
+
+                    os.makedirs(MODELS_DIR, exist_ok=True) # Double check it exists
+                    self.strategy.save_model(os.path.join(MODELS_DIR, f"model_{self.strategy.model_snapshot}.pkl"))
+                    self.strategy.last_save_time = time.time()
+                    self.strategy.model_snapshot += 1
+
+                # if results:
+                #     self.calculate_precision(results, batch_of_packets, results[-1].get("status"))
 
     def report(self, results):
         if not results:
@@ -67,7 +85,7 @@ class ZMQWorker:
         if status == "WARMUP":
             return
         for r, dp in zip(results, data_points[0].get("datapoints")):
-            print(r, dp)
+            # print(r, dp)
             if r.get("is_anomaly") and dp.get("shouldbeAnomaly"):
                 print(f"True Positive: Detected {r['val']} as anomaly, which is correct.")
                 self.tp += 1
@@ -76,3 +94,6 @@ class ZMQWorker:
                 self.fn += 1
         precision = self.tp / (self.tp + self.fn) if (self.tp + self.fn) > 0 else 0
         print(f"Precision: {precision:.2f} (TP: {self.tp}, `FN: {self.fn})")
+
+    def save_model(self, path: str):
+        self.strategy.save_model(path)
